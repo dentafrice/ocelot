008bb4d8e56a52bea5c8cef45bb9fcd938728b57
Caleb Mingle <mingle@uber.com>
2015-07-18 13:07:17 -0400

    Setup operations structure

---
+++ ocelot/pipeline/operations/__init__.py
@@ -0,0 +1,7 @@
+from ocelot.pipeline.operations.xml_parse_operation import XMLParseOperation
+from ocelot.pipeline.operations.xml_rss_parse_operation import XMLRSSParseOperation
+
+__all__ = [
+    XMLParseOperation,
+    XMLRSSParseOperation,
+]
