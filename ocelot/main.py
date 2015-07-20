ba8befc96e7d1ddde433e5558caeab08dac8d656
Caleb Mingle <me@caleb.io>
2015-07-20 00:12:12 -0400

    Update demo

--- ocelot/main.py
+++ ocelot/main.py
@@ -9,51 +9,70 @@ from ocelot.pipeline.channels.operations import XMLParseOperation
 from ocelot.pipeline.channels.operations import XMLRSSParseOperation
 from ocelot.pipeline.channels.outputs import LogOutput
 
+from ocelot.pipeline.plumbing.pipeline import Pipeline
+
+
 if __name__ == '__main__':
-    url = URLInput(
-        url='http://xkcd.com/rss.xml',
+    pipeline = Pipeline(name='XKCD Check')
+
+    # URL Input
+    url = pipeline.add_channel(
+        URLInput(url='http://xkcd.com/rss.xml'),
     )
 
-    change = ChangeFilterOperation(
+    # Change Filter
+    change = pipeline.add_channel(
         # identifier='xkcd-rss',
-        identifier=str(uuid.uuid4()),
+        ChangeFilterOperation(identifier=str(uuid.uuid4()))
     )
 
-    xml = XMLParseOperation()
-    rss = XMLRSSParseOperation()
-    pluck = PluckOperation(
-        fields=['title', 'description'],
-    )
-    pattern = DictPatternExtractor(
-        config={
-            'description': 'src="(.*?)"',
-        }
-    )
-    message_format = MessageFormatOperation(
-        message="""
-        XKCD Lineup:
-        ----
-        {% for item in data %}
-        {{item.title}}
-        {{item.description}}
-        {% endfor %}
-        """,
+    # XML Parser
+    xml = pipeline.add_channel(
+        XMLParseOperation()
     )
 
-    log = LogOutput(log_name='xkcd')
+    # RSS Parser
+    rss = pipeline.add_channel(
+        XMLRSSParseOperation()
+    )
 
-    log.process(
-        message_format.process(
-            pattern.process(
-                pluck.process(
-                    rss.process(
-                        xml.process(
-                            change.process(
-                                url.process(None)
-                            )
-                        )
-                    )
-                )
-            )
+    # Field Plucker
+    plucker = pipeline.add_channel(
+        PluckOperation(fields=['title', 'description'])
+    )
+
+    # Pattern Extractor
+    pattern = pipeline.add_channel(
+        DictPatternExtractor(config={'description': 'src="(.*?)"'})
+    )
+
+    # Message Formatter
+    message = pipeline.add_channel(
+        MessageFormatOperation(
+            message="""
+            XKCD Lineup:
+            ----
+            {% for item in data %}
+            {{item.title}}
+            {{item.description}}
+            {% endfor %}
+            """
         )
     )
+
+    # Log Output
+    log = pipeline.add_channel(
+        LogOutput(log_name='xkcd')
+    )
+
+    # Connections
+    url.connect_fitting(change)
+    change.connect_fitting(xml)
+    xml.connect_fitting(rss)
+    rss.connect_fitting(plucker)
+    plucker.connect_fitting(pattern)
+    pattern.connect_fitting(message)
+    message.connect_fitting(log)
+
+    # Run pipeline
+    pipeline.run()
