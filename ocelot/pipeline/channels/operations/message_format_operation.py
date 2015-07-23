6fb0fdb41e6b15cad446b9ce0c1ee77b1d46a810
Caleb Mingle <me@caleb.io>
2015-07-22 22:47:22 -0700

    MessageFormatOperation uses templating library

--- ocelot/pipeline/channels/operations/message_format_operation.py
+++ ocelot/pipeline/channels/operations/message_format_operation.py
@@ -1,5 +1,4 @@
-from jinja2 import Environment, DictLoader
-
+from ocelot.lib import templating
 from ocelot.pipeline.channels.operations.base_operation import BaseOperation
 
 FAKE_MESSAGE_NAME = 'fake_message_name'
@@ -17,12 +16,9 @@ class MessageFormatOperation(BaseOperation):
         :param data:
         :returns str message:
         """
-        message_environment = Environment(loader=DictLoader({
-            FAKE_MESSAGE_NAME: self.message_value,
-        }))
-
-        message = message_environment.get_template(FAKE_MESSAGE_NAME)
-
-        return message.render({
-            'data': data,
-        })
+        return templating.render_template(
+            template_value=self.message_value,
+            template_variables={
+                'data': data,
+            },
+        )
