c3c4189b0eb965aa9501633f6cbc728dd2651bb2
Caleb Mingle <mingle@uber.com>
2015-08-02 04:05:09 +0000

    Main update

--- ocelot/main.py
+++ ocelot/main.py
@@ -1,6 +1,10 @@
+from ocelot.services import datastores
 from ocelot.services.pipeline import PipelineService
 
 if __name__ == '__main__':
+    datastores.create_tables()
+    datastores.initialize()
+
     PipelineService.run_pipeline_by_id(
         '96feda22-f80d-4cee-ad51-4e18de0b655a',
     )
