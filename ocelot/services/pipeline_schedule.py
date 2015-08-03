8aed8d023483a7137690ded63f4921773cea922f
Caleb Mingle <mingle@uber.com>
2015-08-03 06:31:46 +0000

    PipelineScheduleService handles locking

--- ocelot/services/pipeline_schedule.py
+++ ocelot/services/pipeline_schedule.py
@@ -1,5 +1,8 @@
+from ocelot.lib import logging
 from ocelot.services.mappers.pipeline_schedule import PipelineScheduleMapper
 from ocelot.services.repositories.pipeline_schedule import PipelineScheduleRepository
+
+log = logging.getLogger('ocelot.pipeline_schedules')
 
 
 class PipelineScheduleService(object):
@@ -16,6 +19,82 @@ class PipelineScheduleService(object):
                 pipeline_id,
             ),
         )
+
+    @classmethod
+    def fetch_schedules_to_run(cls):
+        """Return a list of PipelineScheduleEntities that need to run.
+
+        :returns list: PipelineScheduleEntity
+        """
+        return map(
+            PipelineScheduleMapper.to_entity,
+            PipelineScheduleRepository.fetch_schedules_to_run(),
+        )
+
+    @classmethod
+    def pre_run_schedule(cls, pipeline_id):
+        """Set the schedule to running.
+
+        Calling this will lock the schedule and prevent it from being
+        picked up again by the scheduler.
+
+        :param str pipeline_id:
+        """
+        cls.lock_schedule_for_pipeline(pipeline_id)
+
+    @classmethod
+    def post_run_schedule(cls, pipeline_id):
+        """Set the schedule to not running.
+
+        Calling this will unlock the schedule and allow it to be picked
+        up again by the scheduler.
+
+        It will update the next_run_at time according to the schedule.
+
+        :param str pipeline_id:
+        """
+        # TODO: calculate next run time
+        # TODO: set last_run_time = datetime.utcnow()
+        cls.unlock_schedule_for_pipeline(pipeline_id)
+
+    @classmethod
+    def lock_schedule_for_pipeline(cls, pipeline_id):
+        """Locks a schedule for a pipeline.
+
+        :param str pipeline_id:
+        """
+        cls.update_schedule_for_pipeline(
+            pipeline_id,
+            {'locked': True},
+        )
+
+    @classmethod
+    def unlock_schedule_for_pipeline(cls, pipeline_id):
+        """Unlocks a schedule for a pipeline.
+
+        :param str pipeline_id:
+        """
+        cls.update_schedule_for_pipeline(
+            pipeline_id,
+            {'locked': False},
+        )
+
+    @classmethod
+    def update_schedule_for_pipeline(cls, pipeline_id, changes):
+        """Updates a schedule for a pipeline.
+        Schedule must already exist.
+
+        :param str pipeline_id:
+        :param dict changes:
+        """
+        log.info('Updating pipeline: {} with changes {}'.format(pipeline_id, changes))
+
+        schedule = cls.fetch_schedule_for_pipeline(pipeline_id)
+
+        for k, v in changes.items():
+            setattr(schedule, k, v)
+
+        cls.write_pipeline_schedule(schedule)
 
     @classmethod
     def write_pipeline_schedule(cls, pipeline_schedule_entity):
--- ocelot/tests/services/test_pipeline_schedule_service.py
+++ ocelot/tests/services/test_pipeline_schedule_service.py
@@ -23,6 +23,68 @@ class TestPipelineScheduleService(DatabaseTestCase):
             PipelineScheduleMapper.to_entity(self.pipeline_schedule_interval),
         )
 
+    @mock.patch.object(PipelineScheduleRepository, 'fetch_schedules_to_run')
+    def test_fetch_schedules_to_run(self, mock_fetch):
+        """Test that schedules that need to run are returned."""
+        mock_fetch.return_value = [
+            self.pipeline_schedule_interval,
+        ]
+
+        self.assertEqual(
+            PipelineScheduleService.fetch_schedules_to_run(),
+            [
+                PipelineScheduleMapper.to_entity(self.pipeline_schedule_interval),
+            ],
+        )
+
+    @mock.patch.object(PipelineScheduleService, 'lock_schedule_for_pipeline')
+    def test_pre_run_schedule_locks(self, mock_lock):
+        """Test that the schedule is locked."""
+        PipelineScheduleService.pre_run_schedule(self.pipeline.id)
+        mock_lock.assert_called_once_with(self.pipeline.id)
+
+    @mock.patch.object(PipelineScheduleService, 'unlock_schedule_for_pipeline')
+    def test_post_run_schedule_unlocks(self, mock_unlock):
+        """Test that the schedule is locked."""
+        PipelineScheduleService.post_run_schedule(self.pipeline.id)
+        mock_unlock.assert_called_once_with(self.pipeline.id)
+
+    @mock.patch.object(PipelineScheduleService, 'update_schedule_for_pipeline')
+    def test_lock_schedule_for_pipeline(self, mock_update):
+        """Test that the schedule gets locked."""
+        PipelineScheduleService.lock_schedule_for_pipeline(self.pipeline.id)
+        mock_update.assert_called_once_with(
+            self.pipeline.id,
+            {'locked': True},
+        )
+
+    @mock.patch.object(PipelineScheduleService, 'update_schedule_for_pipeline')
+    def test_unlock_schedule_for_pipeline(self, mock_update):
+        """Test that the schedule gets unlocked."""
+        PipelineScheduleService.unlock_schedule_for_pipeline(self.pipeline.id)
+        mock_update.assert_called_once_with(
+            self.pipeline.id,
+            {'locked': False},
+        )
+
+    def test_update_schedule_for_pipeline(self):
+        """Test that the schedule gets updated with the requested attributes."""
+        schedule = PipelineScheduleService.fetch_schedule_for_pipeline(self.pipeline.id)
+        self.assertFalse(schedule.locked)
+        self.assertNotEquals(schedule.schedule, '20')
+
+        PipelineScheduleService.update_schedule_for_pipeline(
+            self.pipeline.id,
+            {
+                'locked': True,
+                'schedule': '20',
+            },
+        )
+
+        schedule = PipelineScheduleService.fetch_schedule_for_pipeline(self.pipeline.id)
+        self.assertTrue(schedule.locked)
+        self.assertEquals(schedule.schedule, '20')
+
     @mock.patch.object(PipelineScheduleRepository, 'write_record')
     def test_write_pipeline_schedule(self, mock_write_record):
         """Test that we can write an entity to the repository."""
